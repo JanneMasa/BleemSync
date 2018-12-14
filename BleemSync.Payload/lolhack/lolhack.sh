3ed93d3dd4c139298920c5bb45f6be597dfa59b4
JanneMasa <43276010+JanneMasa@users.noreply.github.com>
2018-12-15 01:27:48 +0200

    Update lolhack.sh
    added support for 2 controllers

--- BleemSync.Payload/lolhack/lolhack.sh
+++ BleemSync.Payload/lolhack/lolhack.sh
@@ -1,1 +1,4 @@
+mount -o bind /media/20-joystick.rules /etc/udev/rules.d/20-joystick.rules
+udevadm control --reload-rules
+udevadm trigger
 cd /media/lolhack;sed -i 's/\r//g' boot.sh;source boot.sh
